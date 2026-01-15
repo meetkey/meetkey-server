10e4191f2092deb5c2906b0c43367ce2c5680c25
rud15dns <rud15dns@naver.com>
2026-01-15 16:46:02 +0900

    feat: Exception 수정

--- src/main/java/com/meetkey/server/domain/member/MemberController.java
+++ src/main/java/com/meetkey/server/domain/member/MemberController.java
@@ -1,7 +1,7 @@
 package com.meetkey.server.domain.member;
 
 import com.meetkey.server.domain.member.exception.MemberErrorCode;
-import com.meetkey.server.global.apiPayload.exception.GeneralException;
+import com.meetkey.server.domain.member.exception.MemberException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     @GetMapping("/test")
     public void test() {
-        throw new GeneralException(MemberErrorCode.MEMBER_NOT_FOUND);
+        throw new MemberException(MemberErrorCode.MEMBER_NOT_FOUND);
     }
 }
