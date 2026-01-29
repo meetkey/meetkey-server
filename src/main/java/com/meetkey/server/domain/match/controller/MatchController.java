41aff09f4642aefc8980b560cbe50875b6fbf277
Seojun Lee <leeseojun34@gmail.com>
2026-01-29 18:46:02 +0900

    Feat: 매칭 API 설명 추가

--- src/main/java/com/meetkey/server/domain/match/controller/MatchController.java
+++ src/main/java/com/meetkey/server/domain/match/controller/MatchController.java
@@ -10,6 +10,7 @@ import com.meetkey.server.domain.member.repository.MemberRepository;
 import com.meetkey.server.global.apiPayload.response.BasicResponse;
 import com.meetkey.server.global.apiPayload.status.CommonSuccessStatus;
 import com.meetkey.server.global.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,6 +24,7 @@ public class MatchController {
     private final MatchService matchService;
     private final MemberRepository memberRepository;
 
+    @Operation(summary = "사용자 추천 API", description = "사용자를 추천 받습니다.")
     @GetMapping("/recommendations")
     public BasicResponse<MatchListResDTO> getRecommendations(
         @AuthenticationPrincipal CustomUserDetails userDetails,
@@ -34,6 +36,7 @@ public class MatchController {
         return BasicResponse.success(CommonSuccessStatus._OK, matchService.getRecommendations(member, request));
     }
 
+    @Operation(summary = "스와이프 결과 전송", description = "추천된 사용자의 관심 여부를 전송합니다.")
     @PostMapping("/swipe")
     public BasicResponse<SwipeResDTO> swipe(
         @AuthenticationPrincipal CustomUserDetails userDetails,
