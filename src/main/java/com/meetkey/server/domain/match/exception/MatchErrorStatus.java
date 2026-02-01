eb9664a5e155c84a6a1870790c752f56eb348922
Seojun Lee <leeseojun34@gmail.com>
2026-02-01 18:55:36 +0900

    fix: 404 에러 메시지 응답 수정

--- src/main/java/com/meetkey/server/domain/match/exception/MatchErrorStatus.java
+++ src/main/java/com/meetkey/server/domain/match/exception/MatchErrorStatus.java
@@ -13,8 +13,9 @@ public enum MatchErrorStatus implements BaseCode {
     ALREADY_SWIPED(HttpStatus.BAD_REQUEST, "MATCH4002", "이미 스와이프한 상대입니다."),
 
     // 404 Not Found
-    LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "MATCH4005", "위치 정보를 찾을 수 없습니다."),
-    TARGET_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MATCH4006", "대상을 찾을 수 없습니다.");
+    LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "MATCH4041", "위치 정보를 찾을 수 없습니다."),
+    TARGET_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MATCH4042", "대상을 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MATCH4043", "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
